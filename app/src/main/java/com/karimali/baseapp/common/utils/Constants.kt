b4cd8eab7c05404e5624b3cb60fdc99173eca271
Karim Ali <karim.ali14199314@gmail.com>
2023-12-04 21:10:57 +0200

    design app according to MVVM design pattern

---
+++ app/src/main/java/com/karimali/baseapp/common/utils/Constants.kt
@@ -0,0 +1,49 @@
+package com.karimali.baseapp.common.utils
+
+
+object Constants {
+
+    object DeepLinks {
+
+    }
+
+    object Links {
+
+    }
+
+    object Ads {
+    }
+
+    object COLOR {
+    }
+
+    object Preferences {
+        // Preference Comment
+        // You can use MODE_NIGHT_FOLLOW_SYSTEM or MODE_NIGHT_NO or MODE_NIGHT_YES as default value for dark mode.
+        const val DARK_MODE_KEY_DEFAULT_VALUE = "MODE_NIGHT_NO"
+        const val LANGUAGE_KEY = "language"
+        const val DARK_MODE_KEY = "dark_mode"
+        const val RATE_KEY = "rate"
+        const val SHARE_APP_KEY = "share_app"
+        const val DISABLE_ADS_KEY = "disable_ads"
+        const val VERSION_KEY = "version"
+        const val LAUNCH_COUNT_KEY = "launch_ad_count"
+    }
+
+    object Tags {
+        const val LANGUAGE_CHANGED_BY_USER_FLAG_TAG = "LANGUAGE_CHANGED_BY_USER_FLAG"
+        const val SHOULD_SHOW_ON_BOARDING_TO_USER_FOR_FIRST_TIME = "SHOULD_SHOW_ON_BOARDING_TO_USER_FOR_FIRST_TIME"
+        const val SUBSCRIBE_KEY = "subscribe"
+        const val MAIN_SELECTED_TAB_TAG_ID = "selectedTab"
+        const val SELECTED_CONTENT_ID = "selectedContent"
+        const val TRACK_ID = "8"
+        const val SETTING_TAB = 1
+        const val AD_TYPE = 2
+        const val CONTENT_TYPE = 1
+    }
+
+    object Actions {
+
+    }
+
+}
--- app/src/main/java/com/karimali/baseapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/karimali/baseapp/ui/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.karimali.baseapp
+package com.karimali.baseapp.ui.activities
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.karimali.baseapp.R
 import com.karimali.baseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
