21687973ff7cd9fd77ee77f0ab5feba647129c93
Karim Ali <karim.ali14199314>
2024-01-01 23:58:18 +0200

    init splash screen and welcome screen with design

---
+++ app/src/main/java/com/karimali/baseapp/ui/fragments/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.karimali.baseapp.ui.fragments
+
+
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.lifecycle.lifecycleScope
+import com.karimali.baseapp.R
+import com.karimali.baseapp.databinding.FragmentSplashScreenBinding
+import com.karimali.baseapp.ui.base.BaseFragment
+import kotlinx.coroutines.delay
+
+
+class SplashScreen : BaseFragment<FragmentSplashScreenBinding>
+    (FragmentSplashScreenBinding::inflate,R.layout.fragment_splash_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding!!.image.startAnimation(AnimationUtils.loadAnimation(requireContext(),R.anim.alpha_scal_anim))
+
+        lifecycleScope.launchWhenResumed {
+            delay(3000)
+        }
+
+    }
+}
--- app/src/main/java/com/karimali/baseapp/ui/fragments/auth/WelcomeScreen.kt
+++ app/src/main/java/com/karimali/baseapp/ui/fragments/auth/WelcomeScreen.kt
@@ -1,15 +1,17 @@
-package com.karimali.baseapp.ui.fragments
+package com.karimali.baseapp.ui.fragments.auth
 
 import android.os.Bundle
 import android.view.View
 import com.karimali.baseapp.R
-import com.karimali.baseapp.databinding.FragmentHomeBinding
+import com.karimali.baseapp.databinding.FragmentWelcomeScreenBinding
 import com.karimali.baseapp.ui.base.BaseFragment
 
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate,R.layout.fragment_home) {
+class WelcomeScreen : BaseFragment<FragmentWelcomeScreenBinding>
+    ( FragmentWelcomeScreenBinding::inflate,R.layout.fragment_welcome_screen) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        navController
+
     }
 }
