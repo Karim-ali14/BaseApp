29c4b6bf7358e7a28e62528b643c31eae5293bc4
Karim Ali <karim.ali14199314>
2024-01-06 22:20:12 +0200

    init OTP verfication screen design

---
+++ app/src/main/java/com/karimali/baseapp/ui/fragments/auth/VerificationScreen.kt
@@ -0,0 +1,18 @@
+package com.karimali.baseapp.ui.fragments.auth
+
+import android.os.Bundle
+import android.view.View
+import com.karimali.baseapp.R
+import com.karimali.baseapp.databinding.FragmentVerificationScreenBinding
+import com.karimali.baseapp.ui.base.BaseFragment
+
+
+class VerificationScreen : BaseFragment<FragmentVerificationScreenBinding>
+    (FragmentVerificationScreenBinding::inflate,R.layout.fragment_verification_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
